4963700e0f6563559b8dc3d47c63a74291ad9349
john dunne <johnjdunneone@yahoo.ie>
2018-11-18 18:38:51 +0000

    processing input from casino

--- index.py
+++ index.py
@@ -1,13 +1,11 @@
 import time
 import sys
-list = ['start']
-game = ['game-start']
+list = ['']
+game = ['']
 my_5_card_hand = []
 round = set()
 myset = set()
-flop_flag = True
-turn_flag = True
-river_flag = True
+flag_list = [True, True, True]
 
 
 card_numbers = [
@@ -22,20 +20,21 @@ card_numbers = [
 	'Ah', 'Ac', 'As', 'Ad']
 
 def get_river_card(cards):
+    flag_list[2] = False
     idx_1 = cards.find('R')
     idx_2 = cards.find('R',idx_1+1)
     river_cd = cards[idx_1+1:idx_2]
     my_5_card_hand.append(card_numbers[int(river_cd)])
-    return False
 
 def get_turn_card(cards):
+    flag_list[1] = False
     idx_1 = cards.find('T')
     idx_2 = cards.find('T',idx_1+1)
     turn_cd = cards[idx_1+1:idx_2]
     my_5_card_hand.append(card_numbers[int(turn_cd)])
-    return False
 
 def get_flop_cards(flop):
+    flag_list[0] = False
     idx_1 = flop.find('F')
     idx_2 = flop.find('F',idx_1+1)
     idx_3 = flop.find('F',idx_2+1)
@@ -48,7 +47,6 @@ def get_flop_cards(flop):
     my_5_card_hand.append(card_numbers[int(flop_cd3)])
     hand_decision = open('../pokercasino/botfiles/botToCasino0','wt')
     print('r',file=hand_decision)
-    return False
 
 def first_round(my_hand):
     index_1 = my_hand.find('A')
@@ -61,16 +59,26 @@ def first_round(my_hand):
     hand_decision = open('../pokercasino/botfiles/botToCasino0','wt')
     print('c',file=hand_decision)
 
+def make_flop_bet(my_5_cards):
+    outfile = open('home.txt','wt')
+    hand = '-'.join(my_5_cards)
+
+    print(hand,file=outfile)
+    outfile.close()
+
+
 def get_round_num(casinoInfo):
     idx = casinoInfo.find('D')
     round_num = casinoInfo[0:idx]
 
     if round_num not in round:
-        #my_5_card_hand.clear()
+        my_5_card_hand.clear()
         myset.clear()
         round.add(round_num)
-        return [True, True, True]
-    return [False,False,False]
+        flag_list[0]= True
+        flag_list[1]= True
+        flag_list[2]= True
+
 
 
 try:
@@ -78,38 +86,37 @@ try:
 
         infile = open('../pokercasino/botfiles/casinoToBot0','rt')
         first_two_cards = open('../pokercasino/botfiles/cardToBot0','rt')
-        outfile = open('home.txt','wt')
 
-        for line in infile:
-            casinoInfo = str(line.rstrip())
-            flop_flag,turn_flag,river_flag = get_round_num(casinoInfo)
-            if list[len(list)-1] != casinoInfo:
-
-                print(casinoInfo, file=outfile)
-                list.append(casinoInfo)
-                myset = set(casinoInfo)
-
-                if 'F' in myset and flop_flag:
-                    flop_flag = get_flop_cards(casinoInfo)
-                if 'T' in myset and turn_flag:
-                    turn_flag = get_turn_card(casinoInfo)
-                if 'R' in myset and river_flag:
-                    river_flag = get_river_card(casinoInfo)
-
-
-        outfile.close()
 
         for liner in first_two_cards:
             if game[len(game)-1] != str(liner.rstrip()):
+                get_round_num(str(liner.rstrip()))
                 game.append(liner.rstrip())
                 first_round(str(liner.rstrip()))
 
-        time.sleep(0.05)
+        for line in infile:
+            casinoInfo = str(line.rstrip())
+            get_round_num(casinoInfo)
+            if len(list[len(list)-1]) != len(casinoInfo):
+
+                list.append(casinoInfo)
+                myset = set(casinoInfo)
+
+                if 'F' in myset and flag_list[0]:
+                    flag_flop = get_flop_cards(casinoInfo)
+                    make_flop_bet(my_5_card_hand)
+
+                if 'T' in myset and flag_list[1]:
+                    flag_turn = get_turn_card(casinoInfo)
+                if 'R' in myset and flag_list[2]:
+                    flag_river = get_river_card(casinoInfo)
+
 
 except KeyboardInterrupt:
     print('Quitting the program.')
 except:
     print('Unexpected error: '+sys.exc_info()[0])
+
 
 for n in my_5_card_hand:
     print(n)
--- sand.py
+++ sand.py
@@ -1,9 +1,1 @@
-import re
 
-myset = set('john')
-
-myset  = set('frog')
-for n in myset :
-    print(n)
-
-print()
